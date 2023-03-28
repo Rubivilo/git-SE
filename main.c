958fa63ab92a6004b511ab96f9eb1872fba3aa82
Rubivilo <monito.r@live.com>
2023-03-28 18:24:24 +0200

    a
    a

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ void delay(void)
 {
   volatile int i;
 
-  for (i = 0; i < 1000000; i++);
+  for (i = 0; i < 10; i++);
 }
 
 // LED_GREEN = PTD5
@@ -18,7 +18,7 @@ void led_green_init()
   SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
   PORTD->PCR[5] = PORT_PCR_MUX(1);
   GPIOD->PDDR |= (1 << 5);
-  GPIOD->PSOR = (1 << 5);
+  GPIOD->PSOR |= (1 << 5);
 }
 
 void led_green_toggle()
@@ -33,28 +33,28 @@ void led_red_init()
   SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
   PORTE->PCR[29] = PORT_PCR_MUX(1);
   GPIOE->PDDR |= (1 << 29);
-  GPIOE->PSOR = (1 << 29);
+  GPIOE->PSOR |= (1 << 29);
 }
 
 void led_red_toggle(void)
 {
-  GPIOE->PTOR = (1 << 29);
+  GPIOE->PTOR |= (1 << 29);
 }
 
 
 void green_led_off()
 {
-	GPIOD->PSOR = (1 << 5);
+	GPIOD->PSOR |= (1 << 5);
 }
 
 void red_led_off()
 {
-	GPIOE->PSOR = (1 << 29);
+	GPIOE->PSOR |= (1 << 29);
 }
 
 void green_led_on()
 {
-	GPIOD->PCOR = (1 << 5);
+	GPIOD->PCOR |= (1 << 5);
 }
 
 void red_led_on()
@@ -66,7 +66,7 @@ void ptc3_push_button_init()
 {
 	SIM->COPC = 0;										
 	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;					
-	GPIOC->PDDR |= (0 << 3);								
+	GPIOC->PDDR &= ~(1 << 3);								
 	PORTC->PCR[3] |= (PORT_PCR_MUX(1) | PORT_PCR_PE_MASK);
 
 	PORTC->PCR[3] |= PORT_PCR_IRQC(0xA);
@@ -77,7 +77,7 @@ void ptc12_push_button_init()
 {
 	SIM->COPC = 0;										
 	SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-	GPIOC->PDDR |= (0 << 12);
+	GPIOC->PDDR &= ~(1 << 12);
 	PORTC->PCR[12] |= (PORT_PCR_MUX(1) | PORT_PCR_PE_MASK);
 
 	PORTC->PCR[12] |= PORT_PCR_IRQC(0xA);

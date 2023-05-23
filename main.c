2412aff0fa24bcf74f90d18bbe8cca2ca1d426e6
Rubivilo <monito.r@live.com>
2023-05-23 17:37:02 +0200

    EJERCICIO_2-completado
    no funciona bien pero paso al siguiente

--- main.c
+++ main.c
@@ -65,11 +65,37 @@ int verde=0, rojo=0;
 /*******************************************************************************
  * Code
  ******************************************************************************/
- void delay(void)
+void irclk_ini()
+{
+  MCG->C1 = MCG_C1_IRCLKEN(1) | MCG_C1_IREFSTEN(1);
+  MCG->C2 = MCG_C2_IRCS(0); //0 32KHZ internal reference clock; 1= 4MHz irc
+}
+
+void delay(void)
 {
   volatile int i;
 
   for (i = 0; i < 1000000; i++);
+}
+
+void enable_TPM()
+{
+  SIM->SCGC6 |= SIM_SCGC6_TPM0_MASK; // Darlle reloxo ao TPM
+  
+  SIM->SOPT2 |= SIM_SOPT2_TPMSRC(0x03); // MCGIRCLK 
+  TPM0->SC = TPM_SC_CMOD(0x00); // TPM counter is disabled
+  TPM0->SC |= TPM_SC_CPWMS(0x00); // Up-counting
+  TPM0->SC |= TPM_SC_PS(0x00); // Prescale divide by 1
+  TPM0->MOD = TPM_MOD_MOD(0x7CFF); // max counter
+  TPM0->SC |= TPM_SC_TOF_MASK; // Mask 
+  TPM0->SC |= TPM_SC_CMOD(0x01); // TPM enabled the counter increments on every TPM counter clock
+}
+
+void enableInterrupt()
+{
+  // Activate interruptions on ports C e D;
+  NVIC_SetPriority(PORTC_PORTD_IRQn,0);
+  NVIC_EnableIRQ(PORTC_PORTD_IRQn);
 }
  
  // LED_GREEN = PTD5
@@ -122,8 +148,17 @@ void led_red_toggle()
 {
   GPIOE->PTOR = (1 << 29);
 }
-// LED_RED = PTE29
-// LED_GREEN = PTD5
+
+int sw1_check()
+{
+  return( !(GPIOC->PDIR & (1 << 3)) );
+}
+
+int sw2_check()
+{
+  return( !(GPIOC->PDIR & (1 << 12)) );
+}
+
 void leds_ini()
 {
   SIM->COPC = 0;
@@ -136,56 +171,54 @@ void leds_ini()
   GPIOD->PSOR = (1 << 5);
   GPIOE->PSOR = (1 << 29);
 }
-//comun a ambos botones
-void sws_ini(){
-  SIM->COPC = 0;             
-  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-  NVIC_SetPriority(31, 0);  
-  NVIC_EnableIRQ(31);  
-}
 
 // RIGHT_SWITCH (SW1) = PTC3
 void sw1_ini()
 {
-  PORTC->PCR[3] |= PORT_PCR_MUX(1); 
-  PORTC->PCR[3] |= PORT_PCR_PE_MASK;
-  PORTC->PCR[3] |= PORT_PCR_PS_MASK;
-
-  PORTC->PCR[3] |= PORT_PCR_IRQC(0xA);    
+  SIM->COPC = 0;
+  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+  PORTC->PCR[3] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1);
+  GPIOC->PDDR &= ~(1 << 3);
+  PORTC->PCR[3] |= PORT_PCR_IRQC(10); 
 }
 
 // LEFT_SWITCH (SW2) = PTC12
 void sw2_ini()
 {
-  PORTC->PCR[12] |= PORT_PCR_MUX(1); 
-  PORTC->PCR[12] |= PORT_PCR_PE_MASK;
-  PORTC->PCR[12] |= PORT_PCR_PS_MASK;
-
-  PORTC->PCR[12] |= PORT_PCR_IRQC(0xA);   
+  SIM->COPC = 0;
+  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+  PORTC->PCR[12] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1);
+  GPIOC->PDDR &= ~(1 << 12);
+  PORTC->PCR[12] |= PORT_PCR_IRQC(10); 
 }
 
-void PORTDIntHandler(void) {
-  int pressed_switch = PORTC->ISFR;
-  PORTC->ISFR = 0xFFFFFFFF; // Clear IRQ
 
-  // SW1
-  if(pressed_switch == (0x8)) {
+void PORTDIntHandler(void) {
+  PORTC->ISFR = PORT_ISFR_ISF_MASK; // Clear IRQ
+// SW1
+  if(sw1_check()) {
     turn_green_led_on();
     turn_red_led_off();
-    verde++;
-    rojo=0;
   }
   // SW2
-  if(pressed_switch == (0x1000)) {
+  if(sw2_check()) {
     turn_red_led_on();
     turn_green_led_off();
-    rojo++;
-    verde=0;
   }
 }
 int main(void)
 {
   char ch;
+
+  enableInterrupt();
+  sw1_ini();
+  sw2_ini();
+  leds_ini();
+  led_green_ini();
+  led_red_ini();
+
+  irclk_ini(); // Enable internal ref clk to use by LCD
+  enable_TPM();
 
   /* Init board hardware. */
   BOARD_InitPins();
